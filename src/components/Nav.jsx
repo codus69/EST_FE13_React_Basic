2feda9821bed9ef8e1e812ae90efe8fadbae390f
이채연 <196144872+codus69@users.noreply.github.com>
2026-06-23 14:48:48 +0900

    feat: 리액트 프로젝트 기준 경로 변경 및 시작

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+//Nav 컴포넌트
+function Nav({ data }) {
+  const lists = data.map((d) => (
+    <li key={d.id}>
+      <a href="">{d.title}</a>
+    </li>
+  ));
+  //list에 출력할 코드 생성
+  return (
+    <nav>
+      <ul>{lists}</ul>
+    </nav>
+  );
+}
+export default Nav;
